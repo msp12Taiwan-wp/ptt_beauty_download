f53f158adc18398457e5c143f5ecdf6b90db417b
hope51607 <hope51607@yahoo.com.tw>
2018-01-21 16:59:14 +0800

    split

--- img_preprocess.py
+++ img_preprocess.py
@@ -3,7 +3,6 @@
 
 # In[1]:
 
-
 from sklearn import cluster, datasets
 import cv2
 from matplotlib import pyplot as plt
@@ -16,25 +15,24 @@ from scipy import misc,ndimage
 
 from scipy import misc,ndimage
 import matplotlib.pyplot as plt
-# scipy.ndimage
-f = misc.face()
-f = misc.imread('2195_1_8.jpg')
-# blurred_face = ndimage.gaussian_filter(f, sigma=3)
-img = cv2.imread('2196_8_18.jpg')
-plt.imshow(img)
-plt.show()
+# # scipy.ndimage
+# f = misc.face()
+# f = misc.imread('2195_1_8.jpg')
+# # blurred_face = ndimage.gaussian_filter(f, sigma=3)
+# img = cv2.imread('2196_8_18.jpg')
+# plt.imshow(img)
+# plt.show()
 
 
 # In[3]:
-
 
 img = misc.imread('newDownload/2196_4_10.jpg')
 sqrimg=img[0:img.shape[1],0:img.shape[1]]
 # height, width =sqrimg.shape[:2]
 res = cv2.resize(sqrimg,(256, 256), interpolation = cv2.INTER_AREA)
 blured = cv2.blur(res,(5,5))  
-mask = np.zeros((256+2, 256+2), np.uint8) 
-cv2.floodFill(blured, mask, (256-1,256-1), (255,255,255), (2,2,2),(3,3,3),8)
+# mask = np.zeros((256+2, 256+2), np.uint8) 
+# ret,blured,mask,ret=cv2.floodFill(blured, mask, (256-1,256-1), (255,255,255), (2,2,2),(3,3,3),8)
 plt.imshow(blured)
 plt.show()
 blured.shape
--- split_face.py
+++ split_face.py
@@ -5,25 +5,53 @@ from os.path import isfile, isdir, join
 facepPsitions=open('face_position.json','r')
 
 # 指定要列出所有檔案的目錄
-mypath = "newDownload"
+mypath = "/www/LaravelBeauty/public/images"
+
+
+def split_face (mypath,filename,x, y):
+    image=cv2.imread(join(mypath,filename))
+    # print(image)
+    scale=1.2
+    h=int(h*scale)
+    w=int(w*scale)
+    eps=(scale-1)/2
+    if y-h*eps<0:
+        y=0
+    else:
+        y=int(y-h*eps)
+    if x-w*eps<0:
+        x=0
+    else:
+        x=int(x-w*eps)
+    face=image[y:y+h,x:x+w]
+    cv2.resize
+    cv2.imwrite(join('~/trainingData/face',filename),face)
+
+def preprocess(mypath,filename,x, y):
+    image=cv2.imread(join(mypath,filename))
+    # print(image)
+    length=min(image.shape[0],image.shape[1])
+    if length+y>image.shape[0]:
+        y=image.shape[0]-length
+    if length+x>image.shape[1]:
+        x=image.shape[1]-length
+    if y-length/2<0:
+        y=0
+    else:
+        y= int(y-length/2)
+    if x-length/2<0:
+        x=0
+    else:
+        x=int(x-length/2)
+    preprocessImage=image[y:y+length,x:x+length]
+    cv2.resize
+    cv2.imwrite(join('~/trainingData/entireImage',filename),preprocessImage)
+
+
 
 for j in facepPsitions.readlines():
-    print(j)
     face=json.loads(j)
     (x, y, w, h)=(face['faceRectangle']['left'],face['faceRectangle']['top'],face['faceRectangle']['width'],face['faceRectangle']['height'])
     filename=face['faceId']
-    # print(join(mypath,filename))
-    image=cv2.imread(join(mypath,filename))
-    # print(image)
-    h=int(h*1.1)
-    w=int(w*1.1)
-    if y-h*0.05<0:
-        y=0
-    else:
-        y=int(y-h*0.05)
-    if x-w*0.05<0:
-        x=0
-    else:
-        x=int(x-w*0.05)
-    face=image[y:y+h,x:x+w]
-    cv2.imwrite(join(mypath+'/face',filename),face)
+    split_face(mypath,filename,x, y)
+    preprocess(mypath,filename,int(x+w/2),int(y+h/2))
