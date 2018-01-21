b3a8dd2f6cffed0dfcad521d183f813ef55f7d32
hope51607 <hope51607@yahoo.com.tw>
2018-01-21 17:25:15 +0800

    ya

--- split_face.py
+++ split_face.py
@@ -23,7 +23,7 @@ def split_face (mypath,filename,x, y,w,h):
     else:
         x=int(x-w*eps)
     face=image[y:y+h,x:x+w]
-    cv2.resize(face,(128,128))
+    face=cv2.resize(face,(128,128))
     print(join('/home/hausung1998/trainingData/face',filename))
     cv2.imwrite(join('/home/hausung1998/trainingData/face',filename),face)
 
@@ -44,7 +44,7 @@ def preprocess(mypath,filename,x, y):
     else:
         x=int(x-length/2)
     preprocessImage=image[y:y+length,x:x+length]
-    cv2.resize(preprocessImage,(256,256))
+    preprocessImage=cv2.resize(preprocessImage,(256,256))
     cv2.imwrite(join('/home/hausung1998/trainingData/entireImage',filename),preprocessImage)
 
 
